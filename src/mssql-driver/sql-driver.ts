e59dcd8ecf67270fd123f8515b779cbc99f49834
vkhanna <kryptik.1983@gmail.com>
2024-09-07 12:30:39 -0400

    add comment for future ref to self

--- src/mssql-driver/sql-driver.ts
+++ src/mssql-driver/sql-driver.ts
@@ -3,7 +3,7 @@ import { DataSourceOptions, DataSource } from "typeorm";
 
 const msSqlDataSourceOptions: DataSourceOptions = {
   type: "mssql",
-  driver: msSqlDriver,
+  driver: msSqlDriver, //NOTE: this is key, langchainjs has a weird issue working with the ms-sql driver in the package, replacing driver
   host: "localhost",
   port: 1433,
   username: "sa",
