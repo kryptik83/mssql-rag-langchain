87fd00fab5f6803bc9135ade301018b6c3e8c43d
vkhanna <kryptik.1983@gmail.com>
2024-09-09 16:57:08 -0400

    add lint/prettier

--- index.ts
+++ index.ts
@@ -1,30 +1,30 @@
-import { SqlDatabase } from "langchain/sql_db";
-import { createSqlQueryChain, SqlDatabaseChain } from "langchain/chains/sql_db";
-
-import openAi from "./src/config/open-ai.js";
-import msSqlDataSourceOptions from "./src/mssql-driver/sql-driver.js";
-import { testDbConnection } from "./src/utils/connection-tests.js";
+import { SqlDatabaseChain } from 'langchain/chains/sql_db';
+import { SqlDatabase } from 'langchain/sql_db';
+import openAi from './src/config/open-ai.js';
+import msSqlDataSourceOptions from './src/mssql-driver/sql-driver.js';
+import { testDbConnection } from './src/utils/connection-tests.js';
 
 async function main() {
   await testDbConnection();
 
   const langChaindb = await SqlDatabase.fromOptionsParams({
-    appDataSourceOptions: msSqlDataSourceOptions,
+    appDataSourceOptions: msSqlDataSourceOptions
   });
 
   const tableInfo: string = await langChaindb.getTableInfo();
-  console.log(tableInfo);
+  console.log('LangChaingDB connection check', Boolean(tableInfo) ? 'successful' : 'fail');
 
   console.log(langChaindb.allTables.map((a: any) => a.tableName));
 
   const sqlDbChain = new SqlDatabaseChain({
     llm: openAi,
     database: langChaindb,
-    verbose: false,
+    verbose: true
   });
 
+  const questions: string[] = ['How many movies are there?', 'What is the highest rated movie?'];
   const result = await sqlDbChain.invoke({
-    query: "How many movies are there?",
+    query: questions[1]
   });
   // const result = await sqlDbChain.run("How many movies are there?");
   console.log(result);
