ddbf01e19728f606d549b03bc53db9db10c7f09e
vkhanna <kryptik.1983@gmail.com>
2024-09-10 23:58:51 -0400

    program specific code

--- index.ts
+++ index.ts
@@ -64,7 +64,6 @@ async function main(showDebug: boolean = false) {
 
 async function mainLargeDatabase(showDebug: boolean = false) {
   console.log(colors.bold.green('Chatbot for SQL RAG'));
-  console.log(process.env.OPENAI_API_KEY);
 
   console.log(colors.bgWhite.bold.cyan('Configuring your bot'));
   await testDbConnection(showDebug);
@@ -100,7 +99,7 @@ Remember to include ALL POTENTIALLY RELEVANT tables, even if you're not sure tha
 
   const tableGen2Chain = RunnableSequence.from([
     {
-      input: i => i.names
+      input: (i: { question: string }) => i.question
     },
     tableChain
   ]);
@@ -143,5 +142,98 @@ Remember to include ALL POTENTIALLY RELEVANT tables, even if you're not sure tha
   }
 }
 
+async function mainQueryGia(showDebug: boolean = false) {
+  console.log(colors.bold.green('Chatbot for SQL RAG'));
+
+  console.log(colors.bgWhite.bold.cyan('Configuring your bot'));
+  await testDbConnection(showDebug);
+
+  const dbToQuery = await SqlDatabase.fromOptionsParams({
+    appDataSourceOptions: msSqlDataSourceOptions
+  });
+
+  const Table = z.object({
+    names: z.array(z.string()).describe('Names of tables in SQL database')
+  });
+
+  const llm = chatOpenAi;
+
+  const categoryPrompt = ChatPromptTemplate.fromMessages([
+    [
+      'system',
+      `Return the names of the SQL tables that are relevant to the user question.
+  The tables are:
+  
+  Assessment
+  Scope`
+    ],
+    ['human', '{input}']
+  ]);
+
+  const getTables = (categories: z.infer<typeof Table>): string[] => {
+    let tables: string[] = [];
+    for (const category of categories.names) {
+      if (category === 'Scope') {
+        tables = tables.concat(['Alumni', 'AuditClient', 'ODSSI']);
+      } else if (category === 'Assessment') {
+        tables = tables.concat(['Assessment']);
+      }
+    }
+    return tables;
+  };
+
+  const categoryChain = categoryPrompt
+    .pipe(llm.withStructuredOutput(Table)) // Stage 1 uses category prompt to get categories
+    .pipe(getTables); // Stage 2 pipeline converts categories to table names
+
+   const tableChain = RunnableSequence.from([
+     {
+       input: (i: { question: string }) => i.question
+     },
+     categoryChain
+   ]);
+
+  const sqlQueryChain = await createSqlQueryChain({
+    llm: llm,
+    db: dbToQuery,
+    dialect: 'mssql'
+  });
+
+  const fullDbChain = RunnablePassthrough.assign({
+    tableNamesToUse: tableChain
+  }).pipe(sqlQueryChain);
+
+  console.log(colors.bold.green('You can now ask questions using natural language to the bot'));
+
+  while (true) {
+    try {
+      const userInput = readline.question(colors.yellow('You: '));
+
+      let output;
+      if (userInput.toLowerCase() === 'exit') {
+        output = 'Goodbye! If you have any more questions in the future, feel free to ask.';
+        console.log(colors.green('Bot: ') + output);
+        console.log();
+        process.exit();
+      }
+
+      if (showDebug) {
+        console.log(await categoryChain.invoke({ input: userInput }));
+      }
+
+      output = await fullDbChain.invoke({
+        question: userInput
+      });
+
+      console.log(colors.gray('OpenAI: ') + output);
+      return;
+      console.log(colors.green('Bot: ') + (await dbToQuery.run(output)));
+    } catch (error: any) {
+      console.error(colors.bgRed.white(error));
+    }
+  }
+}
+
 // main(false);
-mainLargeDatabase();
+// mainLargeDatabase();
+mainQueryGia(true);
